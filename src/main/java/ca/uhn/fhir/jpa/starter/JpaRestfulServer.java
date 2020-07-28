b060b33166364ec2ed4fb8a1d319e195cf40d69f
Shubham Parikh <shubhamparikh927@gmail.com>
2020-07-28 19:35:39 +0530

    SAP-1667 Added interceptor for authorization (#5)
    * SAP-1667 Added interceptor for authorization
    
    * SAP-1667 Removed unused space.
    
    * SAP-1667 Added newline at the end of the file.
    
    Co-authored-by: Shubham Parikh <>

--- src/main/java/ca/uhn/fhir/jpa/starter/JpaRestfulServer.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/JpaRestfulServer.java
@@ -5,6 +5,8 @@ import javax.servlet.ServletException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 
+import ca.uhn.fhir.rest.server.interceptor.auth.AuthorizationInterceptor;
+
 import javax.servlet.ServletException;
 
 @Import(AppProperties.class)
@@ -25,8 +27,9 @@ public class JpaRestfulServer extends BaseJpaRestfulServer {
   @Override
   protected void initialize() throws ServletException {
     super.initialize();
-
     // Add your own customization here
+    AuthorizationInterceptor authorizationInterceptor = new CustomAuthorizationInterceptor();
+    this.registerInterceptor(authorizationInterceptor);
   }
 
 }
