3c2276fe120a0ba16e8d2391260c2291f84a9613
shubhamparikh927 <sparikh@elimu.io>
2021-02-24 12:45:42 +0530

    Changes in code as per PR suggestions

--- src/main/java/ca/uhn/fhir/jpa/starter/JpaRestfulServer.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/JpaRestfulServer.java
@@ -1,8 +1,13 @@
 package ca.uhn.fhir.jpa.starter;
 
-import ca.uhn.fhir.rest.server.interceptor.auth.AuthorizationInterceptor;
 import javax.servlet.ServletException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+
+import ca.uhn.fhir.rest.server.interceptor.auth.AuthorizationInterceptor;
+
+@Import(AppProperties.class)
 public class JpaRestfulServer extends BaseJpaRestfulServer {
 
   private static final long serialVersionUID = 1L;
