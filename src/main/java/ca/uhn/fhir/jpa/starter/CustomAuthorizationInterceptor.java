f834bd47a630907140589330b21a4e24aa52ccc3
shubhamparikh927 <sparikh@elimu.io>
2021-03-09 17:38:41 +0530

    Added user role validation in Oauth

--- src/main/java/ca/uhn/fhir/jpa/starter/CustomAuthorizationInterceptor.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/CustomAuthorizationInterceptor.java
@@ -1,6 +1,9 @@
 package ca.uhn.fhir.jpa.starter;
 
 import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.apache.commons.lang3.ObjectUtils;
