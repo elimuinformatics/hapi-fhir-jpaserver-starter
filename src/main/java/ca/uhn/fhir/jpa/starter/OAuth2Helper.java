acebf977a7ec7d6c982934dc42c92d788cc9045e
shubhamparikh927 <shubhamparikh927@gmail.com>
2021-07-02 18:23:00 +0530

    Updated code as per PR suggestions

--- src/main/java/ca/uhn/fhir/jpa/starter/OAuth2Helper.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/OAuth2Helper.java
@@ -14,6 +14,7 @@ import org.apache.commons.lang3.ObjectUtils;
 import java.util.Base64;
 import java.util.HashMap;
 
+import org.apache.commons.lang3.ObjectUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
