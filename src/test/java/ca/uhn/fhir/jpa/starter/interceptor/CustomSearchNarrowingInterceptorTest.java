86727454b2ffc409d7bdd1d47bf38a74ab7d74dc
vsinghht <vpanhale@heaptrace.com>
2023-08-31 15:34:03 +0530

    Updated the tests to test resource search

--- src/test/java/ca/uhn/fhir/jpa/starter/interceptor/CustomSearchNarrowingInterceptorTest.java
+++ src/test/java/ca/uhn/fhir/jpa/starter/interceptor/CustomSearchNarrowingInterceptorTest.java
@@ -1,69 +1,205 @@
 package ca.uhn.fhir.jpa.starter.interceptor;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mockStatic;
 
-import java.io.FileNotFoundException;
-import java.lang.reflect.Field;
+import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import org.junit.jupiter.api.Assertions;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hl7.fhir.r4.model.Observation;
+import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.Resource;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.mockito.InjectMocks;
+import org.mockito.MockedStatic;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
-import org.springframework.http.HttpHeaders;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-
+import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.jpa.starter.AppProperties;
 import ca.uhn.fhir.jpa.starter.AppProperties.Oauth;
+import ca.uhn.fhir.jpa.starter.util.OAuth2Helper;
+import ca.uhn.fhir.model.api.IQueryParameterOr;
+import ca.uhn.fhir.model.api.IQueryParameterType;
+import ca.uhn.fhir.rest.annotation.OptionalParam;
+import ca.uhn.fhir.rest.annotation.Search;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import ca.uhn.fhir.rest.param.BaseAndListParam;
+import ca.uhn.fhir.rest.param.ReferenceAndListParam;
+import ca.uhn.fhir.rest.param.TokenAndListParam;
+import ca.uhn.fhir.rest.server.IResourceProvider;
 import ca.uhn.fhir.rest.server.interceptor.auth.AuthorizedList;
+import ca.uhn.fhir.test.utilities.server.RestfulServerExtension;
 
-public class CustomSearchNarrowingInterceptorTest {
+class CustomSearchNarrowingInterceptorTest {
+
+	@Spy
+	AppProperties mockConfig;
+
+	@Spy
+	Oauth mockOAuth;
 
 	@InjectMocks
-	CustomSearchNarrowingInterceptor searchNarrowingInterceptor;
+	CustomSearchNarrowingInterceptor ourSearchNarrowingInterceptor;
 
-	@Spy
-	AppProperties mockConfig = new AppProperties();
+	private static Logger logger = LoggerFactory.getLogger(CustomSearchNarrowingInterceptorTest.class);
+	private static List<Resource> ourReturn;
+	private static String ourLastHitMethod;
+	private static TokenAndListParam ourLastIdParam;
+	private static ReferenceAndListParam ourLastPatientParam;
+	private static final FhirContext ourCtx = FhirContext.forR4();
+	private static final String PATIENT_ID = "12345";
+	public IGenericClient myClient;
 
-	@Spy
-	Oauth mockOAuth = new Oauth();
-
-	private static final String TOKEN_WITH_PATIENT_CLAIM = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjM4YTcyY2E1M2MyMjMxZTA1NjJhNGJjYTgxZDA0OWQ1In0.eyJwYXRpZW50IjoiNjc1MDI5In0.ARILmchVkIJBYm1QWnrrcGdYRnjTf-_hl6g8V5u1ZKK00gwC_-OxpB71LrH2yQ2vNVagsZeTn6XO43zuH7f96mjpQY18ZLSj-IKIxow52eLDlPkVl6YjB2ntmXj_dEOCUt_DeXeUil3LXCPW1Y2PA4XSu_wTFlIN75bnD1a68n8ky4iBMtuzhEdSvxoU0DawpmtTSpqIqkDR5Kp_sWkEjpplMNhgv0GpQPlQvdVJL60R7KjGm8pf5NdsZ36NFEthQYAFzVDcC-nq_Rm8EsIYjtjv5c9naWRzC3etDEjEjAjQxoNqIZxCULSoJ16f3MQopoXQO-kA3ejxOqK0fYu_ShKgZjkT6VQ4_d66NxQsImLHe8fdLimgp4LISuv6UQXhBximCPzN2S3_pdPEvZWorIvEojrrJrZIdIxxpyM700IhGALh-GVa_3OSWbMuIrPs3TT9iU8ZhWFWHlcGM95PseJCR-BcrL5xE7kvr2HtjTId-anlzjw3x0qxPbD4bk8w";
-	private static final String TOKEN_WITHOUT_PATIENT_CLAIM = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjM4YTcyY2E1M2MyMjMxZTA1NjJhNGJjYTgxZDA0OWQ1In0.e30.PN2ej0hqZejJW2yGV8LjaVZwNCrinN9N8D6FhdzBQHPD8h0RwqRVLbqfZHD_p8QrRJYL_sJ2tbqxWIuWDSyVEUQehLC-p6KoyUao1Id-PCuejovASeZGmbX9NkalW6s4JPl2cc7sMyg6mU_aGTiyhaTDWtjULemv8Fm6GPNFIYURibIwUsbWhxq0-544vb9t9eWFP5KLj9aa6D1HUxq12l9Md9zfhm0X8v5EIgFH2S1XY-fkIIEryl8R3kgtVRkmElyN2ZgdtTrt9u7bWX_Zm2gnIgiP1omXGpptCyhimlhJ-wohmw-ULjvpUv4GYxEJn5Olhr0vQPPmapg3pgdNR4AnFWL3YTZVeSwZuef4FxHRzVWv5qXf8Yajai3EanxMASM_xxAsuKsFbJ15ehQPGerKIQJPT7_cMakL4f0ohdYZrxsRspzxAWDYTPfsrFtZVIiTi3JiAB6f82OF4T2krKtJVzyjKFYXYmy3XINg2W6-ldBFQ3Y-6lb3oZjM5Ejd";
+	@RegisterExtension
+	public RestfulServerExtension myRestfulServerExtension = new RestfulServerExtension(ourCtx)
+			.registerProvider(new MockPatientResourceProvider())
+			.registerProvider(new MockObservationResourceProvider());
 
 	@BeforeEach
-	public void before() throws FileNotFoundException {
+	public void before() throws IOException {
 		MockitoAnnotations.openMocks(this);
+		ourReturn = Collections.emptyList();
+		ourLastHitMethod = null;
+		ourLastIdParam = null;
+		ourLastPatientParam = null;
+		mockOAuth.setEnabled(false);
+		mockConfig.setOauth(mockOAuth);
+		myClient = myRestfulServerExtension.getFhirClient();
 	}
 
 	@Test
-	void buildAuthorizedListWithPatientClaim() throws JsonProcessingException, IllegalArgumentException,
-			IllegalAccessException, NoSuchFieldException, SecurityException {
-		RequestDetails mockRequestDetails = mock(RequestDetails.class);
+	void testSearchNarrowObservationWithPatientClaim() throws Exception {
 		mockOAuth.setEnabled(true);
-		when(mockConfig.getOauth()).thenReturn(mockOAuth);
-		when(mockRequestDetails.getHeader(HttpHeaders.AUTHORIZATION)).thenReturn("Bearer " + TOKEN_WITH_PATIENT_CLAIM);
-		AuthorizedList rule = searchNarrowingInterceptor.buildAuthorizedList(mockRequestDetails);
-		Field privateField = AuthorizedList.class.getDeclaredField("myAllowedCompartments");
-		privateField.setAccessible(true);
-		List<String> fieldValue = (List<String>) privateField.get(rule);
-		Assertions.assertEquals("Patient/675029", fieldValue.get(0));
+		ourSearchNarrowingInterceptor = new CustomSearchNarrowingInterceptor(mockConfig) {
+			@Override
+			protected AuthorizedList buildAuthorizedList(RequestDetails theRequestDetails) {
+				try (MockedStatic<OAuth2Helper> mockedStatic = mockStatic(OAuth2Helper.class)) {
+					mockedStatic.when(() -> OAuth2Helper.hasToken(any())).thenReturn(true);
+					mockedStatic.when(() -> OAuth2Helper.getClaimAsString(any(RequestDetails.class), any())).thenReturn(PATIENT_ID);
+					return super.buildAuthorizedList(theRequestDetails);
+				}
+			}
+		};
+		myRestfulServerExtension.registerInterceptor(ourSearchNarrowingInterceptor);
+		myClient.search().forResource("Observation").execute();
+		logger.info("Patient Reference Param : {}", ourLastPatientParam);
+
+		assertEquals("Observation.search", ourLastHitMethod);
+		assertEquals("Patient/" + PATIENT_ID, toStrings(ourLastPatientParam).get(0));
 	}
 
 	@Test
-	void buildAuthorizedListWithoutPatientClaim() throws JsonProcessingException, IllegalArgumentException,
-			IllegalAccessException, NoSuchFieldException, SecurityException {
-		RequestDetails mockRequestDetails = mock(RequestDetails.class);
+	void testSearchNarrowObservationWithoutPatientClaim() throws Exception {
 		mockOAuth.setEnabled(true);
-		when(mockConfig.getOauth()).thenReturn(mockOAuth);
-		when(mockRequestDetails.getHeader(HttpHeaders.AUTHORIZATION))
-				.thenReturn("Bearer " + TOKEN_WITHOUT_PATIENT_CLAIM);
-		AuthorizedList rule = searchNarrowingInterceptor.buildAuthorizedList(mockRequestDetails);
-		Assertions.assertTrue(rule instanceof AuthorizedList);
+		ourSearchNarrowingInterceptor = new CustomSearchNarrowingInterceptor(mockConfig) {
+			@Override
+			protected AuthorizedList buildAuthorizedList(RequestDetails theRequestDetails) {
+				try (MockedStatic<OAuth2Helper> mockedStatic = mockStatic(OAuth2Helper.class)) {
+					mockedStatic.when(() -> OAuth2Helper.hasToken(any())).thenReturn(true);
+					mockedStatic.when(() -> OAuth2Helper.getClaimAsString(any(RequestDetails.class), any())).thenReturn(null);
+					return super.buildAuthorizedList(theRequestDetails);
+				}
+			}
+		};
+		myRestfulServerExtension.registerInterceptor(ourSearchNarrowingInterceptor);
+		myClient.search().forResource("Observation").execute();
+		logger.info("Patient Reference Param : {}", ourLastPatientParam);
+
+		assertEquals("Observation.search", ourLastHitMethod);
+		assertNull(ourLastPatientParam);
+	}
+
+	@Test
+	void testSearchNarrowPatientWithPatientClaim() throws Exception {
+		mockOAuth.setEnabled(true);
+		ourSearchNarrowingInterceptor = new CustomSearchNarrowingInterceptor(mockConfig) {
+			@Override
+			protected AuthorizedList buildAuthorizedList(RequestDetails theRequestDetails) {
+				try (MockedStatic<OAuth2Helper> mockedStatic = mockStatic(OAuth2Helper.class)) {
+					mockedStatic.when(() -> OAuth2Helper.hasToken(any())).thenReturn(true);
+					mockedStatic.when(() -> OAuth2Helper.getClaimAsString(any(RequestDetails.class), any())).thenReturn(PATIENT_ID);
+					return super.buildAuthorizedList(theRequestDetails);
+				}
+			}
+		};
+		myRestfulServerExtension.registerInterceptor(ourSearchNarrowingInterceptor);
+		myClient.search().forResource("Patient").execute();
+		logger.info("Patient Id Param : {}", ourLastIdParam);
+
+		assertEquals("Patient.search", ourLastHitMethod);
+		assertEquals("Patient/" + PATIENT_ID, toStrings(ourLastIdParam).get(0));
+	}
+
+	@Test
+	void testSearchNarrowPatientWithoutPatientClaim() throws Exception {
+		mockOAuth.setEnabled(true);
+		ourSearchNarrowingInterceptor = new CustomSearchNarrowingInterceptor(mockConfig) {
+			@Override
+			protected AuthorizedList buildAuthorizedList(RequestDetails theRequestDetails) {
+				try (MockedStatic<OAuth2Helper> mockedStatic = mockStatic(OAuth2Helper.class)) {
+					mockedStatic.when(() -> OAuth2Helper.hasToken(any())).thenReturn(true);
+					mockedStatic.when(() -> OAuth2Helper.getClaimAsString(any(RequestDetails.class), any())).thenReturn(null);
+					return super.buildAuthorizedList(theRequestDetails);
+				}
+			}
+		};
+		myRestfulServerExtension.registerInterceptor(ourSearchNarrowingInterceptor);
+		myClient.search().forResource("Patient").execute();
+		logger.info("Patient Id Param : {}", ourLastIdParam);
+
+		assertEquals("Patient.search", ourLastHitMethod);
+		assertNull(ourLastIdParam);
+	}
+
+	private List<String> toStrings(BaseAndListParam<? extends IQueryParameterOr<?>> theParams) {
+		List<? extends IQueryParameterOr<? extends IQueryParameterType>> valuesAsQueryTokens = theParams
+				.getValuesAsQueryTokens();
+
+		return valuesAsQueryTokens.stream().map(IQueryParameterOr::getValuesAsQueryTokens)
+				.map(t -> t.stream().map(j -> j.getValueAsQueryToken(ourCtx)).collect(Collectors.joining(",")))
+				.collect(Collectors.toList());
+	}
+
+	public static class MockObservationResourceProvider implements IResourceProvider {
+
+		@Override
+		public Class<? extends IBaseResource> getResourceType() {
+			return Observation.class;
+		}
+
+		@Search()
+		public List<Resource> search(@OptionalParam(name = "_id") TokenAndListParam theIdParam,
+				@OptionalParam(name = Observation.SP_PATIENT) ReferenceAndListParam thePatientParam) {
+			ourLastHitMethod = "Observation.search";
+			ourLastIdParam = theIdParam;
+			ourLastPatientParam = thePatientParam;
+			return ourReturn;
+		}
+	}
+
+	public static class MockPatientResourceProvider implements IResourceProvider {
+
+		@Override
+		public Class<? extends IBaseResource> getResourceType() {
+			return Patient.class;
+		}
+
+		@Search()
+		public List<Resource> search(@OptionalParam(name = "_id") TokenAndListParam theIdParam) {
+			ourLastHitMethod = "Patient.search";
+			ourLastIdParam = theIdParam;
+			return ourReturn;
+		}
 	}
 }
