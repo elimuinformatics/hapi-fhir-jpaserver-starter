3f0f6514b38d6c2d0c5450ae525dc5b6ef68c770
yashmohod <ymohod@elimu.io>
2025-04-02 20:04:58 +0530

    RT-1968: Add launnch id and app

--- src/main/java/ca/uhn/fhir/jpa/starter/interceptor/LoggerInterceptor.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/interceptor/LoggerInterceptor.java
@@ -17,30 +17,49 @@ import ca.uhn.fhir.rest.api.server.ResponseDetails;
 import ca.uhn.fhir.rest.server.servlet.ServletRequestDetails;
 
 @Interceptor
-public class CorrelationIdInterceptor {
+public class LoggerInterceptor {
   private static final String X_CORRELATION_ID = "X-Correlation-Id";
+  private static final String X_LAUNCH_ID = "X-Launch-Id";
+  private static final String X_APP_NAME = "X-App-Name";
+  private static final String LAUNCH_ID = "launchId";
+  private static final String APP = "app";
+
 
   @Hook(Pointcut.SERVER_INCOMING_REQUEST_PRE_PROCESSED)
   public boolean incomingRequestPreProcessed(HttpServletRequest request,
       HttpServletResponse response) {
-    String corelationId = ObjectUtils.isNotEmpty(request.getHeader(X_CORRELATION_ID))
+	 String correlationId = ObjectUtils.isNotEmpty(request.getHeader(X_CORRELATION_ID))
         ? request.getHeader(X_CORRELATION_ID)
         : UUID.randomUUID().toString();
-    MDC.put(X_CORRELATION_ID, corelationId);
-    return true;
+	MDC.put(X_CORRELATION_ID, correlationId);
+		putIfPresent(request, X_LAUNCH_ID, LAUNCH_ID);
+		putIfPresent(request, X_APP_NAME, APP);
+
+		return true;
   }
+
+	private void putIfPresent(HttpServletRequest request, String headerName, String mdcKey) {
+		String headerValue = request.getHeader(headerName);
+		if (ObjectUtils.isNotEmpty(headerValue)) {
+			MDC.put(mdcKey, headerValue);
+		}
+	}
 
   @Hook(Pointcut.SERVER_OUTGOING_RESPONSE)
   public boolean outgoingResponse(RequestDetails requestDetails,
       ServletRequestDetails servletRequestDetails, IBaseResource resource, ResponseDetails responseDetails,
       HttpServletRequest request, HttpServletResponse response) {
     response.addHeader(X_CORRELATION_ID, MDC.get(X_CORRELATION_ID));
-    return true;
+	  String launchId = MDC.get(LAUNCH_ID);
+	  if (launchId != null) {
+		  response.addHeader(LAUNCH_ID, launchId);
+	  }
+	  return true;
   }
 
   @Hook(Pointcut.SERVER_PROCESSING_COMPLETED)
   public void processingCompleted(RequestDetails requestDetails,
       ServletRequestDetails servletRequestDetails) {
-    MDC.remove(X_CORRELATION_ID);
+	  MDC.clear();
   }
 }
